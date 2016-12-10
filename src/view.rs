ef0e7219903d7c60c49ddc384c67c2784eb30148
Michael Walker <mike@barrucadu.co.uk>
2016-12-10 03:31:23 +0000

    Avoid mut variable in renderer.

--- src/view.rs
+++ src/view.rs
@@ -1,4 +1,4 @@
-use model::{X, Y, WorldData, Static, Mobile};
+use model::*;
 
 use pancurses::Window;
 
@@ -6,24 +6,29 @@ impl WorldData {
     pub fn render(&self, window: &Window) {
         for row_n in 0..Y {
             for col_n in 0..X {
-                let mut ch = ' ';
-                if let Some(mob) = self.mobiles[row_n][col_n] {
-                    ch = match mob {
-                        Mobile::Player => '@',
-                        Mobile::Fiend => 'f',
-                        Mobile::Arrow => '/'
-                    };
-                } else if let Some(stat) = self.statics[row_n][col_n] {
-                    ch = match stat {
-                        Static::Wall => '#',
-                        Static::Gate => if row_n == 0 || row_n == Y-1 { '-' } else { '|' },
-                        Static::Goal => 'Y',
-                        Static::Turret => 'O',
-                        Static::Obstacle => '=',
-                    };
-                }
+                let ch = self.mobiles[row_n][col_n]
+                    .map_or(self.statics[row_n][col_n]
+                            .map_or(' ', |s| self.render_static(row_n, s)), |m| self.render_mobile(m));
                 window.mvaddch(row_n as i32, col_n as i32, ch);
             }
+        }
+    }
+
+    pub fn render_mobile(&self, mob: Mobile) -> char {
+        match mob {
+            Player => '@',
+            Fiend => 'f',
+            Arrow => '/'
+        }
+    }
+
+    pub fn render_static(&self, row_n: usize, stat: Static) -> char {
+        match stat {
+            Wall => '#',
+            Gate => if row_n == 0 || row_n == Y-1 { '-' } else { '|' },
+            Goal => 'Y',
+            Turret => 'O',
+            Obstacle => '=',
         }
     }
 }
