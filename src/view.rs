0d6417334bb02f6f39db0cafbfecd48a2d7979a4
Michael Walker <mike@barrucadu.co.uk>
2016-12-10 04:03:12 +0000

    Render turrets bold.

--- src/view.rs
+++ src/view.rs
@@ -1,13 +1,13 @@
 use model::*;
 
-use pancurses::Window;
+use pancurses::{A_BOLD, ToChtype, Window, chtype};
 
 impl WorldData {
     pub fn render(&self, window: &Window) {
         for row_n in 0..Y {
             for col_n in 0..X {
                 let ch = self.mobiles[row_n][col_n].map_or(self.statics[row_n][col_n]
-                                                               .map_or(' ', |s| {
+                                                               .map_or(' '.to_chtype(), |s| {
                                                                    self.render_static(row_n, s)
                                                                }),
                                                            |m| self.render_mobile(m));
@@ -16,37 +16,41 @@ impl WorldData {
         }
     }
 
-    pub fn render_mobile(&self, mob: Mobile) -> char {
+    pub fn render_mobile(&self, mob: Mobile) -> chtype {
         match mob {
-            Player => '@',
-            Fiend => 'f',
-            Arrow { dx, dy } => {
-                if dx == 0 {
-                    '|'
-                } else if dy == 0 {
-                    '-'
-                } else if (dx < 0 && dy < 0) || (dy > 0 && dx > 0) {
-                    '\\'
-                } else {
-                    '/'
+                Player => '@',
+                Fiend => 'f',
+                Arrow { dx, dy } => {
+                    if dx == 0 {
+                        '|'
+                    } else if dy == 0 {
+                        '-'
+                    } else if (dx < 0 && dy < 0) || (dy > 0 && dx > 0) {
+                        '\\'
+                    } else {
+                        '/'
+                    }
                 }
             }
-        }
+            .to_chtype()
     }
 
-    pub fn render_static(&self, row_n: usize, stat: Static) -> char {
-        match stat {
-            Wall => '#',
-            Gate => {
-                if row_n == 0 || row_n == Y - 1 {
-                    '-'
-                } else {
-                    '|'
+    pub fn render_static(&self, row_n: usize, stat: Static) -> chtype {
+        let chty = match stat {
+                Wall => '#',
+                Gate => {
+                    if row_n == 0 || row_n == Y - 1 {
+                        '-'
+                    } else {
+                        '|'
+                    }
                 }
+                Goal => 'Y',
+                Turret => 'O',
+                Obstacle => '=',
             }
-            Goal => 'Y',
-            Turret => 'O',
-            Obstacle => '=',
-        }
+            .to_chtype();
+
+        if stat == Turret { chty | A_BOLD } else { chty }
     }
 }
