ef944f9ff5b4d133348d1c3a8cb6cc605d68817e
Nathan van Doorn <nvd1234@gmail.com>
2016-12-10 02:12:49 +0000

    Start implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate pancurses;
+
+use pancurses::*;
+
+const X: usize = 64;
+const Y: usize = 32;
+
+#[derive(Clone, Copy)]
+struct room_cell{
+
+}
+
+static mut room: [[room_cell; X]; Y] = [[room_cell{}; X]; Y];
+
 fn main() {
-    println!("Hello, world!");
+    let window = initscr();
+    for row_n in 0..Y {
+        for col_n in 0..X {
+            window.mvaddch(row_n as i32, col_n as i32, '+');
+        }
+    }
+    let _ = window.getch();
+    let _ = endwin();
 }
